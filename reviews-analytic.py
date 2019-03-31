58af7aef7dcf144467d74a5013737d254441ba37
Carter Wu <a84682579@gmail.com>
2019-03-31 13:22:26 +0800

    加入備註

--- reviews-analytic.py
+++ reviews-analytic.py
@@ -8,12 +8,12 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-print(len(data))
-print(data[0])
+print(len(data))  #統計data清單裡的留言則數有多少
+print(data[0])  #印出第一則留言
 
-sum_len = 0
-for d in data:
-	sum_len = sum_len + len(d)
+sum_len = 0  #統計每則留言有多少字數
+for d in data:  #for loop 處理每則留言，代號d
+	sum_len = sum_len + len(d)  #len(d)計算每則留言的長度，並加總
 
 print(sum_len / len(data))
 
